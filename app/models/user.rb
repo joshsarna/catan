2781cb5fa5a1043278f415994ba6b36157472e2b
joshsarna <joshsarna@gmail.com>
2020-04-09 10:57:24 -0500

    make game model methods and add to game show

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,31 @@ class User < ApplicationRecord
   has_many :players
   has_many :games, through: :players
   has_many :hands
+
+  def face_up_development_cards(game_id)
+    hand = hands.find_by(game_id: game_id)
+    development_cards = hand.development_card_hands
+    face_up_development_cards = development_cards.where(face_up: true)
+    return face_up_development_cards.map do |development_card_in_hand|
+      development_card_in_hand.development_card
+    end
+  end
+
+  def current_game_hand(game_id)
+    hand = hands.find_by(game_id: game_id)
+    return {
+      wood_count: hand.wood_count,
+      rock_count: hand.rock_count,
+      wheat_count: hand.wheat_count,
+      sheep_count: hand.sheep_count,
+      brick_count: hand.brick_count,
+      development_cards: hand.development_card_hands.map { |development_card_hand|
+        {
+          name: development_card_hand.development_card.name,
+          image_url: development_card_hand.development_card.image_url,
+          face_up: development_card_hand.face_up
+        }
+      }
+    }
+  end
 end
--- app/views/api/games/show.json.jbuilder
+++ app/views/api/games/show.json.jbuilder
@@ -2,3 +2,10 @@ json.id @game.id
 json.current_turn @game.current_turn
 json.current_has_rolled @game.current_has_rolled
 json.last_roll @game.last_roll
+
+json.players @game.users do |user|
+  json.name user.name
+  json.development_cards user.face_up_development_cards(@game.id)
+end
+
+json.hand current_user.current_game_hand(game_id)
